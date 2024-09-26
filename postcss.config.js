846df88969a683b6ca388ba397d207f2fa6dfe8c
luis-tenorio-code <luisprograming0@gmail.com>
2024-09-25 23:01:26 -0500

    fix: :recycle: remove postcss-nesting plugin from configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 import postcssGlobalData from '@csstools/postcss-global-data';
 import cssnano from 'cssnano';
 import postcssCustomMedia from 'postcss-custom-media';
-import postcssNesting from 'postcss-nesting';
 
 export default {
   plugins: [
@@ -9,7 +8,6 @@ export default {
       files: ['./src/styles/custom-media.css']
     }),
     postcssCustomMedia(),
-    postcssNesting(),
     process.env.NODE_ENV === 'production' && cssnano()
   ]
 };
