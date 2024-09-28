6a674804b9e9f068692801afb413608c635254fc
Felix Icaza <fx.joliett17@gmail.com>
2024-09-27 21:09:16 -0600

    feat: enable custom media functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,14 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    css: {
+      transformer: 'lightningcss',
+      lightningcss: {
+        drafts: {
+          customMedia: true
+        }
+      }
+    },
     build: {
       cssMinify: 'lightningcss'
     }
