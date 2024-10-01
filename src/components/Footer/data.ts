33137a73f92b61b16c1c0087b64fa9c1bf80ccd0
Jaime Alan Gutiérrez Cruz <33694882+jagcruz@users.noreply.github.com>
2024-10-01 04:23:25 -0300

    refactor: :recycle: refactor `Footer` component

---
+++ src/components/Footer/data.ts
@@ -0,0 +1,52 @@
+import type { LinkIconItem, LinkItem } from '@contracts';
+
+/**
+ * Elementos del footer
+ */
+export const footerItems: LinkItem[] = [
+  {
+    text: 'Código de conducta',
+    url: '/codigo-de-conducta',
+    label: 'código de conducta'
+  },
+  {
+    text: 'Términos y Condiciones',
+    url: '#',
+    label: 'términos y condiciones'
+  },
+  {
+    text: 'Política de privacidad',
+    url: '#',
+    label: 'política de privacidad'
+  }
+];
+
+/**
+ * Elementos de redes sociales para el footer
+ */
+export const socialItems: LinkIconItem[] = [
+  {
+    text: 'Linkedin',
+    url: 'https://www.linkedin.com/company/jsconf-chile/',
+    icon: 'linkedin',
+    label: 'Linkedin'
+  },
+  {
+    text: 'Instagram',
+    url: 'https://www.instagram.com/jsconf.cl/',
+    icon: 'instagram',
+    label: 'Instagram'
+  },
+  {
+    text: 'Twitter',
+    url: 'https://x.com/jsconfcl',
+    icon: 'twitter',
+    label: 'Twitter'
+  },
+  {
+    text: 'Facebook',
+    url: 'https://www.facebook.com/jsconfcl',
+    icon: 'facebook',
+    label: 'Facebook'
+  }
+];
