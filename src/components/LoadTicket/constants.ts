8104e537ba50d2848776d54c51a33059b3f34766
Jaime Alan Gutiérrez Cruz <33694882+jagcruz@users.noreply.github.com>
2024-10-02 02:01:57 -0300

    refactor: fix paths for model and texture of ThreeJS

--- src/components/LoadTicket/constants.ts
+++ src/components/LoadTicket/constants.ts
@@ -1,4 +1,4 @@
 const baseUrl = import.meta.env.PROD ? `${import.meta.env.SITE}/` : '';
 
-export const TEXTURE = `${baseUrl}/assets/misc/band.png`;
-export const MODEL = `${baseUrl}/assets/misc/ticket.glb`;
+export const TEXTURE = `${baseUrl}/assets/misc/band.png`.replaceAll('//', '/');
+export const MODEL = `${baseUrl}/assets/misc/ticket.glb`.replaceAll('//', '/');
