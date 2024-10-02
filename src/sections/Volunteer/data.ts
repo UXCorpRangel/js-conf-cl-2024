b45df9653e80297dd11c4b6803a89440a36e247b
Jaime Alan Gutiérrez Cruz <33694882+jagcruz@users.noreply.github.com>
2024-10-02 00:56:08 -0300

    refactor: update image paths for volunteer section

--- src/sections/Volunteer/data.ts
+++ src/sections/Volunteer/data.ts
@@ -2,37 +2,37 @@ import type { StaffData } from '@contracts';
 
 export const volunteerData: StaffData[] = [
   {
-    imgSrc: '/volunteer/miguel-colmenarez.avif',
+    imgSrc: '/assets/img/volunteer/miguel-colmenarez.avif',
     staffName: 'Miguel Colmenarez',
     linkedinLink: 'https://www.linkedin.com/in/colmedev/',
     githubLink: 'https://github.com/colmedev'
   },
   {
-    imgSrc: '/volunteer/felix-icaza.avif',
+    imgSrc: '/assets/img/volunteer/felix-icaza.avif',
     staffName: 'Félix Icaza',
     linkedinLink: 'https://linkedin.com/in/felixicaza',
     githubLink: 'https://github.com/felixicaza'
   },
   {
-    imgSrc: '/volunteer/facundo-dandrea.avif',
+    imgSrc: '/assets/img/volunteer/facundo-dandrea.avif',
     staffName: 'Facundo D´Andrea',
     linkedinLink: 'https://www.linkedin.com/in/facundo-d-andrea/',
     githubLink: 'https://github.com/Facundodandrea'
   },
   {
-    imgSrc: '/volunteer/jaime-gutierrez.avif',
+    imgSrc: '/assets/img/volunteer/jaime-gutierrez.avif',
     staffName: 'Jaime Gutiérrez',
     linkedinLink: 'https://linkedin.com/in/jagcruz',
     githubLink: 'https://github.com/jagcruz'
   },
   {
-    imgSrc: '/volunteer/luis-tenorio.avif',
+    imgSrc: '/assets/img/volunteer/luis-tenorio.avif',
     staffName: 'Luis Tenorio',
     linkedinLink: 'https://www.linkedin.com/in/luis-tenorio-code',
     githubLink: 'https://github.com/luis-codex'
   },
   {
-    imgSrc: '/volunteer/ricardo-cuauro.avif',
+    imgSrc: '/assets/img/volunteer/ricardo-cuauro.avif',
     staffName: 'Ricardo Cuauro',
     linkedinLink: 'https://www.linkedin.com/in/ricardocuauro/',
     githubLink: 'https://github.com/odracirdev'
