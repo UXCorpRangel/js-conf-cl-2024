11279e7bedf032be85b55281a6550e2d6a571a0d
Ricardo Cuauro <84311660+odracirdev@users.noreply.github.com>
2024-10-07 19:02:14 -0300

    fix: update hostname

--- apps/jsconf/src/components/LiveStreamWidget/config.ts
+++ apps/jsconf/src/components/LiveStreamWidget/config.ts
@@ -1,6 +1,6 @@
 export const URL_STREAM = 'https://www.youtube.com/live/O8M8h3_lZxA?si=7PD_dEz4f0VjH6zJ';
 
-export const hostname = process.env.NODE_ENV === 'development' ? 'localhost' : 'laprevia.jsconf.cl';
+export const hostname = process.env.NODE_ENV === 'development' ? 'localhost' : 'jsconf.cl';
 
 // sabado 12 october 9:30 hasta 7:00pm (PREVIA) hora chile.
 export const dataLiveStreamWidget = {
