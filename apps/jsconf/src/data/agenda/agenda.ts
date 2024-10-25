0d8d1d59192afffd196f47727e4897caa52690f8
Felix Icaza <fx.joliett17@gmail.com>
2024-10-24 22:04:23 -0600

    refactor: restructure agenda data to object format with day-based organization for better clarity

--- apps/jsconf/src/data/agenda/agenda.ts
+++ apps/jsconf/src/data/agenda/agenda.ts
@@ -1,25 +1,73 @@
-const agenda = [
-  {
-    time_start: '2024-10-25T02:28:00.000Z',
-    time_end: '2024-10-25T02:28:10.000Z',
-    speaker_name: 'Midudev',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, voluptatibus. Quam, quisquam, doloremque. Quidem, consequuntur, velit. Voluptas, quos, fugiat. Tempora, autem, repellendus.'
-  },
-  {
-    time_start: '2024-11-05T09:00:00.000Z',
-    time_end: '2024-11-07T17:00:00.000Z',
-    speaker_name: 'Midudev',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, voluptatibus. Quam, quisquam, doloremque. Quidem, consequuntur, velit. Voluptas, quos, fugiat. Tempora, autem, repellendus.'
-  },
-  {
-    time_start: '2024-11-05T09:00:00.000Z',
-    time_end: '2024-11-07T17:00:00.000Z',
-    speaker_name: 'Midudev',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, voluptatibus. Quam, quisquam, doloremque. Quidem, consequuntur, velit. Voluptas, quos, fugiat. Tempora, autem, repellendus.'
-  }
-];
+const agenda = {
+  friday: [
+    {
+      time_start: '2024-10-25T03:58:00.000Z',
+      time_end: '2024-10-25T03:58:05.000Z',
+      speaker_name: 'Midudev',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, voluptatibus. Quam, quisquam, doloremque. Quidem, consequuntur, velit. Voluptas, quos, fugiat. Tempora, autem, repellendus.'
+    },
+    {
+      time_start: '2024-11-05T09:00:00.000Z',
+      time_end: '2024-11-07T17:00:00.000Z',
+      speaker_name: 'Midudev',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, voluptatibus. Quam, quisquam, doloremque. Quidem, consequuntur, velit. Voluptas, quos, fugiat. Tempora, autem, repellendus.'
+    },
+    {
+      time_start: '2024-11-05T09:00:00.000Z',
+      time_end: '2024-11-07T17:00:00.000Z',
+      speaker_name: 'Midudev',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, voluptatibus. Quam, quisquam, doloremque. Quidem, consequuntur, velit. Voluptas, quos, fugiat. Tempora, autem, repellendus.'
+    }
+  ],
+  saturday: [
+    {
+      time_start: '2024-10-25T03:58:10.000Z',
+      time_end: '2024-10-25T03:58:15.000Z',
+      speaker_name: 'Midudev 1',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, voluptatibus. Quam, quisquam, doloremque. Quidem, consequuntur, velit. Voluptas, quos, fugiat. Tempora, autem, repellendus.'
+    },
+    {
+      time_start: '2024-11-05T09:00:00.000Z',
+      time_end: '2024-11-07T17:00:00.000Z',
+      speaker_name: 'Midudev 1',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, voluptatibus. Quam, quisquam, doloremque. Quidem, consequuntur, velit. Voluptas, quos, fugiat. Tempora, autem, repellendus.'
+    },
+    {
+      time_start: '2024-11-05T09:00:00.000Z',
+      time_end: '2024-11-07T17:00:00.000Z',
+      speaker_name: 'Midudev 1',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, voluptatibus. Quam, quisquam, doloremque. Quidem, consequuntur, velit. Voluptas, quos, fugiat. Tempora, autem, repellendus.'
+    }
+  ],
+  sunday: [
+    {
+      time_start: '2024-10-25T03:58:20.000Z',
+      time_end: '2024-10-25T03:58:25.000Z',
+      speaker_name: 'Midudev 2',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, voluptatibus. Quam, quisquam, doloremque. Quidem, consequuntur, velit. Voluptas, quos, fugiat. Tempora, autem, repellendus.'
+    },
+    {
+      time_start: '2024-11-05T09:00:00.000Z',
+      time_end: '2024-11-07T17:00:00.000Z',
+      speaker_name: 'Midudev 2',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, voluptatibus. Quam, quisquam, doloremque. Quidem, consequuntur, velit. Voluptas, quos, fugiat. Tempora, autem, repellendus.'
+    },
+    {
+      time_start: '2024-11-05T09:00:00.000Z',
+      time_end: '2024-11-07T17:00:00.000Z',
+      speaker_name: 'Midudev 2',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, voluptatibus. Quam, quisquam, doloremque. Quidem, consequuntur, velit. Voluptas, quos, fugiat. Tempora, autem, repellendus.'
+    }
+  ]
+};
 
 export default agenda;
