cf3cdf967498b62439a140753262ae402461d01a
Felix Icaza <fx.joliett17@gmail.com>
2024-10-24 22:06:14 -0600

    refactor: remove outdated agendaData type definition

--- apps/jsconf/src/sections/agenda/index.ts
+++ apps/jsconf/src/sections/agenda/index.ts
@@ -1,8 +1,1 @@
-export type agendaData = {
-  time_start: string;
-  time_end: string;
-  speaker_name: string;
-  description: string;
-};
-
 export { default as ContentTab } from './ContentTab.astro';
